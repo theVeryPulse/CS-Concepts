f5f902b665331ae52baff8f32651c357854c85d9
Philip Li (Junhaoran) <125798752+TheVeryPulse@users.noreply.github.com>
2023-10-21 00:58:22 +0100

    Create ft_putnbr.c

---
+++ ft_putnbr.c
@@ -0,0 +1,49 @@
+#include <limits.h>
+#include <unistd.h>
+
+const char  *g_base_10 = "0123456789";
+
+int abs_val(int num)
+{
+    if (num < 0)
+        return (-num);
+    else
+        return (num);
+}
+
+/*Print a number of integer type*/
+void    ft_putnbr(int nb)
+{
+    char    rev_ary[20];
+    int     i;
+    int     sign;
+
+    i = 0;
+    if (nb >= 0)
+        sign = 1;
+	else
+		sign = -1;
+	while (nb / 10 != 0)
+	{
+		rev_ary[i++] = g_base_10[abs_val(nb % 10)];
+        nb /= 10;
+    }
+    rev_ary[i] = g_base_10[abs_val(nb % 10)];
+    if (sign == -1)
+        write(1, "-", 1);
+    while (i >= 0 && rev_ary[i])
+        write(1, rev_ary + (i--), 1);
+}
+
+int main(void)
+{
+    ft_putnbr(INT_MIN);
+    write(1, "\n", 1);
+    ft_putnbr(INT_MAX);
+    write(1, "\n", 1);
+    ft_putnbr(0);
+    write(1, "\n", 1);
+    ft_putnbr(-1);
+    write(1, "\n", 1);
+    ft_putnbr(1);
+}
