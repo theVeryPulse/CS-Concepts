48f77645d797a7e50cc0b926963d744a03b80182
Philip Li <macpilot23@outlook.com>
2023-11-02 02:10:12 +0000

    ft_strjoin.c

---
+++ in_test/ft_strjoin.c
@@ -0,0 +1,45 @@
+#include <stdlib.h>
+
+static size_t	ft_strlen(const char *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*joined;
+	size_t	i;
+	size_t	j;
+	size_t	len;
+
+	len = ft_strlen(s1) + ft_strlen(s2);
+	joined = (char *)malloc((len + 1) * sizeof(char));
+	if (!joined)
+		return (joined);
+	i = 0;
+	while (s1[i])
+	{
+		joined[i] = s1[i];
+		i++;
+	}
+	j = 0;
+	while (s2[j])
+		joined[i++] = s2[j++];
+	joined[i] = '\0';
+	return (joined);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char *str0 = "str0";
+	char *str1 = "str1";
+	printf("%s\n", ft_strjoin(str0, str1));
+}
+*/
