6d929f14fd44e3f61015347708c30665ffd0d6c7
Philip Li 42 <macpilot23@outlook.com>
2023-11-04 01:09:27 +0000

    Add new files
    Add makefile for compiling
    Improve ft_itoa.c: use functions from other files
    Add ft_strmapi.c
    Add libft.h

--- in_test/ft_itoa.c
+++ in_test/ft_itoa.c
@@ -6,49 +6,18 @@
 /*   By: juli <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/03 17:40:23 by juli              #+#    #+#             */
-/*   Updated: 2023/11/03 19:46:01 by juli             ###   ########.fr       */
+/*   Updated: 2023/11/03 23:37:25 by juli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* tested: 03/11/2023 */
 
 #include <stdlib.h>
+#include "libft.h"
 
-static size_t	ft_strlen(char *str);
-
-static size_t	ft_strlcpy(char *dst, const char *src, size_t size);
-
-static void		re_itoa(long num, char *ary);
-
-static size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+static size_t	*re_itoa(long num, char *ary)
 {
-	size_t	i;
-	size_t	total_len;
-
-	i = 0;
-	total_len = (size_t)ft_strlen((char *)src);
-	while (i < size - 1 && src[i])
-	{
-		dst[i] = src[i];
-		i++;
-	}
-	dst[i] = '\0';
-	return (total_len);
-}
-
-static size_t	ft_strlen(char *str)
-{
-	size_t	len;
-
-	len = 0;
-	while (str[len])
-		len++;
-	return (len);
-}
-
-static void	re_itoa(long num, char *ary)
-{
-	static int	j = 0;
+	static size_t	j = 0;
 
 	if (ary[j] == '-')
 		j++;
@@ -59,6 +28,7 @@ static void	re_itoa(long num, char *ary)
 		re_itoa(num / 10, ary);
 		ary[j++] = num % 10 + '0';
 	}
+	return (&j);
 }
 
 char	*ft_itoa(int n)
@@ -67,6 +37,7 @@ char	*ft_itoa(int n)
 	char	ary[40];
 	char	*out;
 	long	num;
+	size_t	*j;
 
 	i = 0;
 	while (i < 40)
@@ -77,9 +48,10 @@ char	*ft_itoa(int n)
 		num = -num;
 		ary[0] = '-';
 	}
-	re_itoa(num, ary);
+	j = re_itoa(num, ary);
+	*j = 0;
 	out = (char *)malloc((ft_strlen(ary) + 1) * sizeof(char));
-	ft_strlcpy(out, ary, 40);
+	ft_strlcpy(out, ary, (ft_strlen(ary) + 1));
 	return (out);
 }
 /*
@@ -88,7 +60,11 @@ char	*ft_itoa(int n)
 
 int	main(void)
 {
-	int num = 5000;
+	int num = 50000;
+	printf("%d (int)\n%s (char[])\n", num, ft_itoa(num));
+	num = INT_MIN;
+	printf("%d (int)\n%s (char[])\n", num, ft_itoa(num));
+	num = INT_MAX;
 	printf("%d (int)\n%s (char[])\n", num, ft_itoa(num));
 	return (0);
 }
