04a7061ca417c49ff24b94c56f02a9a4f037fb75
Philip Li 42 <macpilot23@outlook.com>
2023-11-05 16:49:54 +0000

    Update libft.h
    Fixed the wrong naming of ft_memmove

--- libft/libft.h
+++ libft/libft.h
@@ -34,7 +34,7 @@ void	ft_bzero(void *s, size_t n);
 
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 
-void	*memmove(void *dest, const void *src, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 
