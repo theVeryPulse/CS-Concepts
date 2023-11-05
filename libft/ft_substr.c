da5f1ff1fdcef132a8d7d116317ef3c2cc540e2a
Philip Li 42 <macpilot23@outlook.com>
2023-11-05 16:43:15 +0000

    Add ft_substr.c

---
+++ libft/ft_substr.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juli <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/05 16:27:11 by juli              #+#    #+#             */
+/*   Updated: 2023/11/05 16:27:13 by juli             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* last tested on 05/11/2023 */
+
+/*	Allocates (with malloc(3)) and returns a substring from the string ’s’.
+	The substring begins at index ’start’ and is of maximum size ’len’.*/
+
+#include <stddef.h>
+#include <stdlib.h>
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*newstr;
+	size_t	i;
+	size_t	newlen;
+
+	if (start >= ft_strlen(s))
+		return (NULL);
+	newlen = ft_strlen(&s[start]);
+	if (newlen > len)
+		newlen = len;
+	i = 0;
+	newstr = (char *)malloc((newlen + 1) * sizeof(char));
+	if (newstr == NULL)
+		return (NULL);
+	while (i < newlen)
+	{
+		newstr[i] = s[start + i];
+		i++;
+	}
+	newstr[i] = '\0';
+	return (newstr);
+}
+/*
+#include <stdio.h>
+
+int	main(void)
+{
+	char *s = "0123456789";
+	unsigned int start = 10;
+	size_t len = 3;
+	printf("%s\n", ft_substr(s, start, len));
+}
+*/
